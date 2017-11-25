dd2485855bff57d9fe25ccf41948872b000e6d5b
willau <willyaws@gmail.com>
2017-11-25 23:30:30 +0100

    initial commit of merge sort

---
+++ algo/merge_sort.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+"""
+author: willy au
+date: 25 Nov. 2017
+
+Time complexity of merge sort algorithm:
+    + Average -> O(n.log(n))
+    + Worst   -> O(n.log(n))
+
+Space complexity, we need an auxiliary array of size n:
+    + O(n)
+"""
